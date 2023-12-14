1d5234702c7113263d1997c57997b86f8658f88a
Baris Yerlikaya <b.yerlikaya@outlook.com>
2023-12-14 20:25:47 +0300

    the received backup file has been zipped.

---
+++ SqlBackupToS3/ZipTool.cs
@@ -0,0 +1,19 @@
+﻿namespace SqlBackupToS3;
+
+public static class ZipTool
+{
+    public static void MakeZipFile(string filePath) => Archive(filePath);
+
+    public static void MakeZipFileAndDelete(string filePath)
+    {
+        Archive(filePath);
+        File.Delete(filePath);
+    }
+
+    private static void Archive(string filePath)
+    {
+        var zipFilePath = $"{filePath}.zip";
+        using var archive = ZipFile.Open(zipFilePath, ZipArchiveMode.Create);
+        archive.CreateEntryFromFile(filePath, Path.GetFileName(filePath));
+    }
+}
