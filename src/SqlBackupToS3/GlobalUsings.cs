639e8aee0f19928b87d43dfb40d43d2eb5d8f10f
Baris Yerlikaya <b.yerlikaya@outlook.com>
2023-12-15 17:07:57 +0300

    Global usings added.

--- src/SqlBackupToS3/GlobalUsings.cs
+++ src/SqlBackupToS3/GlobalUsings.cs
@@ -1,9 +1,20 @@
 // Global using directives
 
+global using AmazonWebServices.Extensions;
+global using AmazonWebServices.Interfaces;
+global using AmazonWebServices.Requests;
 global using Microsoft.Data.SqlClient;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Hosting;
+global using Microsoft.Extensions.Logging;
 global using Microsoft.SqlServer.Management.Common;
 global using Microsoft.SqlServer.Management.Sdk.Sfc;
 global using Microsoft.SqlServer.Management.Smo;
+global using SqlBackupToS3.BackgroundServices;
+global using SqlBackupToS3.Options;
+global using SqlBackupToS3.Utilities;
 global using System;
 global using System.IO;
 global using System.IO.Compression;
+global using System.Threading;
+global using System.Threading.Tasks;
