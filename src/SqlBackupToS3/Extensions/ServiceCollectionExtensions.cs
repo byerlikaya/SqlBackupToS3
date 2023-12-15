9dbd8d330f567fa9baef02a219992ee05ee304f2
Baris Yerlikaya <b.yerlikaya@outlook.com>
2023-12-15 17:05:28 +0300

    ServiceCollectionExtensions has been added.

---
+++ src/SqlBackupToS3/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,43 @@
+﻿namespace SqlBackupToS3.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddSqlBackup(
+        this IServiceCollection services,
+        Action<SqlBackupOption> options)
+    {
+        ArgumentNullControl(services, options);
+
+        SingletonServices(services, options);
+
+        //https://github.com/byerlikaya/AmazonWebServices
+        services.AddAmazonWebServices();
+
+        return services;
+    }
+
+    private static void SingletonServices(
+        IServiceCollection services,
+        Action<SqlBackupOption> options)
+    {
+        services.AddSingleton<SqlTool>();
+        services.AddSingleton<ZipArchiveTool>();
+
+        services.AddSingleton(_ =>
+        {
+            var optionInstance = new SqlBackupOption();
+            options(optionInstance);
+            return optionInstance;
+        });
+
+        services.AddHostedService<BackupBackgroundService>();
+    }
+
+    private static void ArgumentNullControl(
+        IServiceCollection services,
+        Action<SqlBackupOption> options)
+    {
+        if (services is null) throw new ArgumentNullException(nameof(services));
+        if (options is null) throw new ArgumentNullException(nameof(options));
+    }
+}
