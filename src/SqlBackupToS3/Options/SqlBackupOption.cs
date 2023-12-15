5a635677845969e0b44a355c5f6038be5095f1b4
Baris Yerlikaya <b.yerlikaya@outlook.com>
2023-12-15 17:04:33 +0300

    Backup operations are complete.

---
+++ src/SqlBackupToS3/Options/SqlBackupOption.cs
@@ -0,0 +1,26 @@
+﻿namespace SqlBackupToS3.Options;
+
+public class SqlBackupOption
+{
+    private int _dailyRepeat;
+
+    public string FolderPath { get; set; }
+
+    public string ConnectionString { get; set; }
+
+    public int DailyRepeat
+    {
+        get =>
+            _dailyRepeat is default(int)
+            ? 1
+            : _dailyRepeat;
+
+        set =>
+            _dailyRepeat = value switch
+            {
+                <= 0 => 1,
+                > 24 => 24,
+                _ => value
+            };
+    }
+}
