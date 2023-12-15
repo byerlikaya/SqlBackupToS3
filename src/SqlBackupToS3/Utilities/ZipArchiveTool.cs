73e9af33f4d0c03c0892421e441d7eb1eed99056
Baris Yerlikaya <b.yerlikaya@outlook.com>
2023-12-15 17:04:12 +0300

    File zip operations are complete.

---
+++ src/SqlBackupToS3/Utilities/ZipArchiveTool.cs
@@ -0,0 +1,21 @@
+﻿namespace SqlBackupToS3.Utilities;
+
+internal class ZipArchiveTool
+{
+    public string MakeZipFile(string filePath) => Archive(filePath);
+
+    public string MakeZipFileAndDelete(string filePath)
+    {
+        var zipFilePath = Archive(filePath);
+        File.Delete(filePath);
+        return zipFilePath;
+    }
+
+    private string Archive(string filePath)
+    {
+        var zipFilePath = $"{filePath}.zip";
+        using var archive = ZipFile.Open(zipFilePath, ZipArchiveMode.Create);
+        archive.CreateEntryFromFile(filePath, Path.GetFileName(filePath));
+        return zipFilePath;
+    }
+}
