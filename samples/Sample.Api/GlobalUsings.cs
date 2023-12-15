0d4e4a47b5de45be6c89c203f73cdf33f7c87f0a
Baris Yerlikaya <b.yerlikaya@outlook.com>
2023-12-15 17:08:48 +0300

    sample usage added.

---
+++ samples/Sample.Api/GlobalUsings.cs
@@ -0,0 +1,3 @@
+// Global using directives
+
+global using SqlBackupToS3.Extensions;
--- samples/Sample.Api/Program.cs
+++ samples/Sample.Api/Program.cs
@@ -5,6 +5,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddSqlBackup(x =>
+{
+    x.ConnectionString = builder.Configuration.GetConnectionString("SqlServerConnection");
+    x.FolderPath = @"D:\Backup";
+    x.DailyRepeat = 2;
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -16,29 +23,4 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
